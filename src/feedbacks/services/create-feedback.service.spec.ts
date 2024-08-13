c397989631b15bb659bd329b4da1cdd1615e58e1
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-12 21:34:44 -0300

    test: ensure SUT creates a feedback for a product

--- src/feedbacks/services/create-feedback.service.spec.ts
+++ src/feedbacks/services/create-feedback.service.spec.ts
@@ -5,11 +5,22 @@ import { Test } from '@nestjs/testing';
 import { CreateFeedbackService } from './create-feedback.service';
 
 import { type CreateFeedbackDto } from '../dtos/create-feedback.dto';
+import { type Feedback } from '../feedback.entity';
 import { FeedbacksRepository } from '../feedbacks.repository';
 
 describe('CreateFeedbackService', () => {
 	let createFeedbackService: CreateFeedbackService;
 	let feedbacksRepository: FeedbacksRepository;
+
+	const createFeedbackDto: CreateFeedbackDto = {
+		userId: faker.string.uuid(),
+		productId: faker.string.uuid(),
+		comment: faker.lorem.text(),
+		rating: faker.number.int({
+			min: 0,
+			max: 5,
+		}),
+	};
 
 	beforeEach(async () => {
 		const moduleRef = await Test.createTestingModule({
@@ -20,9 +31,6 @@ describe('CreateFeedbackService', () => {
 					useValue: {
 						createOne: jest.fn(),
 						find: jest.fn(),
-						findById: jest.fn(),
-						removeOne: jest.fn(),
-						updateOne: jest.fn(),
 					},
 				},
 			],
@@ -35,22 +43,16 @@ describe('CreateFeedbackService', () => {
 			moduleRef.get<FeedbacksRepository>(FeedbacksRepository);
 	});
 
+	afterAll(() => {
+		jest.clearAllMocks();
+	});
+
 	it('should be defined', () => {
 		expect(createFeedbackService).toBeDefined();
 		expect(feedbacksRepository).toBeDefined();
 	});
 
 	it('should throw if user has already sent a feedback for a product', async () => {
-		const createFeedbackDto: CreateFeedbackDto = {
-			userId: faker.string.uuid(),
-			productId: faker.string.uuid(),
-			comment: faker.lorem.text(),
-			rating: faker.number.int({
-				min: 0,
-				max: 5,
-			}),
-		};
-
 		jest
 			.spyOn(feedbacksRepository, 'find')
 			.mockResolvedValueOnce([{ ...createFeedbackDto }]);
@@ -60,5 +62,25 @@ describe('CreateFeedbackService', () => {
 				`User "${createFeedbackDto.userId} has already sent feedback for product "${createFeedbackDto.productId}".`,
 			),
 		);
+	});
+
+	it('should create a feedback for a product successfully', async () => {
+		const feedback: Feedback = {
+			...createFeedbackDto,
+		};
+
+		jest.spyOn(feedbacksRepository, 'find').mockResolvedValueOnce([]);
+		jest
+			.spyOn(feedbacksRepository, 'createOne')
+			.mockResolvedValueOnce(feedback);
+
+		const result = await createFeedbackService.exec(createFeedbackDto);
+
+		expect(feedbacksRepository.find).toHaveBeenCalledWith({
+			productId: createFeedbackDto.productId,
+			userId: createFeedbackDto.userId,
+		});
+		expect(feedbacksRepository.createOne).toHaveBeenCalledWith(feedback);
+		expect(result).toEqual(feedback);
 	});
 });
