01d749b73c30d327c07936ea85fc0cb142a6f426
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-14 23:07:12 -0300

    chore: use swc instead of ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ export default {
 	moduleFileExtensions: ['js', 'json', 'ts'],
 	testRegex: '.*\\.spec\\.ts$',
 	transform: {
-		'^.+\\.(t|j)s$': 'ts-jest',
+		'^.+\\.(t|j)sx?$': '@swc/jest',
 	},
 	collectCoverageFrom: ['**/*.(t|j)s'],
 	coverageDirectory: '../coverage',
