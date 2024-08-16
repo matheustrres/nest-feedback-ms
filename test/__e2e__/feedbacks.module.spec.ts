00c07b9e8bbf5c87dd43b6a49b68d547f4a96610
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-16 18:45:29 -0300

    test(e2e): ensure route return an error if body has incorret arguments

--- test/__e2e__/feedbacks.module.spec.ts
+++ test/__e2e__/feedbacks.module.spec.ts
@@ -7,8 +7,19 @@ import request from 'supertest';
 import { type CreateFeedbackDto } from '@/feedbacks/dtos/create-feedback.dto';
 import { FeedbacksModule } from '@/feedbacks/feedbacks.module';
 
+import { ZodExceptionFilter } from '@/shared/lib/exceptions/filters/zod-exception.filter';
 import { DatabaseModule } from '@/shared/modules/database/database.module';
 import { DatabaseService } from '@/shared/modules/database/database.service';
+
+const makeDto = () => ({
+	userId: faker.string.uuid(),
+	productId: faker.string.uuid(),
+	comment: faker.lorem.lines(1),
+	rating: faker.number.int({
+		min: 0,
+		max: 5,
+	}),
+});
 
 describe('FeedbacksModule', () => {
 	let app: INestApplication;
@@ -20,6 +31,7 @@ describe('FeedbacksModule', () => {
 		}).compile();
 
 		app = moduleRef.createNestApplication();
+		app.useGlobalFilters(new ZodExceptionFilter());
 		await app.init();
 
 		conn = moduleRef.get<DatabaseService>(DatabaseService).getConnection();
@@ -31,30 +43,14 @@ describe('FeedbacksModule', () => {
 	});
 
 	describe('X POST /feedbacks', () => {
-		const dto: CreateFeedbackDto = {
-			userId: faker.string.uuid(),
-			productId: faker.string.uuid(),
-			comment: faker.lorem.lines(1),
-			rating: faker.number.int({
-				min: 0,
-				max: 5,
-			}),
-		};
-
-		it('should create a feedback', async () => {
-			return request(app.getHttpServer())
-				.post('/feedbacks')
-				.send(dto)
-				.expect(201)
-				.then((res) => {
-					expect(res.body).toMatchObject(dto);
-				});
-		});
-
 		it('should return an error if user has already sent feedback for given product', async () => {
-			await request(app.getHttpServer()).post('/feedbacks').send(dto);
+			const server = app.getHttpServer();
 
-			return request(app.getHttpServer())
+			const dto = makeDto();
+
+			await request(server).post('/feedbacks').send(dto);
+
+			return request(server)
 				.post('/feedbacks')
 				.send(dto)
 				.expect(400)
@@ -62,6 +58,47 @@ describe('FeedbacksModule', () => {
 					message: `User "${dto.userId} has already sent feedback for product "${dto.productId}".`,
 					error: 'Bad Request',
 					statusCode: 400,
+				});
+		});
+
+		it('should return an error if incorret arguments are provided', async () => {
+			return request(app.getHttpServer())
+				.post('/feedbacks')
+				.send({
+					...makeDto(),
+					rating: 10,
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).toMatchObject({
+						status: 'ERROR',
+						statusCode: 400,
+						content: [
+							{
+								code: 'too_big',
+								path: 'rating',
+								message: 'Number must be less than or equal to 5',
+							},
+						],
+						endpoint: 'POST /feedbacks',
+					});
+
+					expect(res.body['timestamp']).toBeDefined();
+				});
+		});
+
+		it('should create a feedback', async () => {
+			const dto = makeDto();
+
+			return request(app.getHttpServer())
+				.post('/feedbacks')
+				.send(dto)
+				.expect(201)
+				.then((res) => {
+					expect(res.body).toMatchObject(dto);
+					expect(res.body['id']).toBeDefined();
+					expect(res.body['createdAt']).toBeDefined();
+					expect(res.body['updatedAt']).toBeDefined();
 				});
 		});
 	});
